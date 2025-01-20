3e1db1cf3ea3366770754e52183eacc71f964be3
Gaurav <thegauravvv@gmail.com>
2025-01-20 22:44:39 +0530

    second commit 20/1

--- extras/mandate_image.py
+++ extras/mandate_image.py
@@ -2,6 +2,9 @@ import os
 from PIL import Image
 import shutil
 from math import floor
+import tempfile
+
+MAXSIZE = 100000
 
 def createJpg(img, maxsize, output):
     img.convert('L').save(output,  optimize=True, quality=55)     #first pass
@@ -16,6 +19,42 @@ def createJpg(img, maxsize, output):
         pass_counter = pass_counter + 1
     
     print(output, pass_counter)
+
+def makeJpg(file):
+    img = Image.open(file)
+    output = tempfile.TemporaryFile()
+    img.convert('L').save(output, format="JPEG",  optimize=True, quality=75)     #first pass
+    size = output.tell()    #get the saved file size
+    pass_counter = 1
+    
+    x = 0
+    while size > MAXSIZE:          #applying successive passes
+        w, h = floor(img.width * (1-x)), floor(img.height * (1-x))
+        img.convert('L').resize((w, h)).save(output, format='JPEG', optimize=True, quality=55)
+        size = output.tell()
+        x = x + 4/100
+        pass_counter += 1
+    
+    print(output, pass_counter)
+    return output
+
+def makeTif(file):
+    img = Image.open(file)
+    output = tempfile.TemporaryFile()
+    img.convert('1').save(output, format="TIFF",  optimize=True)     #first pass
+    size = output.tell()    #get the saved file size
+    pass_counter = 1
+    
+    x = 0
+    while size > MAXSIZE:          #applying successive passes
+        x = x + 4/100
+        w, h = floor(img.width * (1-x)), floor(img.height * (1-x))
+        img.convert('1').resize((w, h)).save(output, format="TIFF",  optimize=True)
+        size = output.tell()
+        pass_counter += 1
+    
+    print(output, pass_counter)
+    return output
     
 def createTif(img, maxsize, output):
     img.convert('1').save(output,  optimize=True)     #first pass
--- extras/mandate_xml.py
+++ extras/mandate_xml.py
@@ -107,3 +107,107 @@ def createXml(outfile, ref, data):
     ET.indent(tree, space='\t')
     tree.write(outfile, encoding='UTF-8', xml_declaration=True)
 
+def makeXml(outfile, ref, data):
+    Document = ET.Element('Document')
+    MndtInitnReq = ET.SubElement(Document, 'MndtInitnReq')
+    
+    GrpHdr = ET.SubElement(MndtInitnReq, 'GrpHdr')
+    MsgId = ET.SubElement(GrpHdr, 'MsgId') #Message Request ID
+    CreDtTm = ET.SubElement(GrpHdr, 'CreDtTm') #some sort of time, at which mandate is created in the system?
+    
+    InstgAgt = ET.SubElement(GrpHdr, 'InstgAgt')
+    FinInstnId1 = ET.SubElement(InstgAgt, 'FinInstnId')
+    ClrSysMmbId1 = ET.SubElement(FinInstnId1, 'ClrSysMmbId')
+    MmbId1 = ET.SubElement(ClrSysMmbId1, 'MmbId') #PUNB0HGB001
+    Nm_creditor_bank_head = ET.SubElement(FinInstnId1, 'Nm') #SARVA HARYANA GRAMIN BANK
+    
+    InstdAgt = ET.SubElement(GrpHdr, 'InstdAgt')
+    FinInstnId2 = ET.SubElement(InstdAgt, 'FinInstnId')
+    ClrSysMmbId2 = ET.SubElement(FinInstnId2, 'ClrSysMmbId')
+    MmbId2 = ET.SubElement(ClrSysMmbId2, 'MmbId') #DEBIT IFSC
+    #Nm_debtor_bank_head = ET.SubElement(FinInstnId2, 'Nm') #DEBIT bank
+    
+    Mndt = ET.SubElement(MndtInitnReq, 'Mndt')
+    MndtReqId = ET.SubElement(Mndt, 'MndtReqId') #Message Request ID
+    
+    Tp = ET.SubElement(Mndt, 'Tp')
+    SvcLvl = ET.SubElement(Tp, 'SvcLvl')
+    Prtry_S = ET.SubElement(SvcLvl, 'Prtry') #L001
+    LclInstrm = ET.SubElement(Tp, 'LclInstrm')
+    Prtry_L = ET.SubElement(LclInstrm, 'Prtry') #DEBIT
+    
+    Ocrncs = ET.SubElement(Mndt, 'Ocrncs')
+    SeqTp = ET.SubElement(Ocrncs, 'SeqTp') #RCUR
+    Frqcy = ET.SubElement(Ocrncs, 'Frqcy') #MNTH
+    Drtn = ET.SubElement(Ocrncs, 'Drtn')
+    FrDt = ET.SubElement(Drtn, 'FrDt') #date of mandate
+    
+    FrstColltnDt = ET.SubElement(Ocrncs, 'FrstColltnDt') #from date
+    FnlColltnDt = ET.SubElement(Ocrncs, 'FnlColltnDt') #to date
+    
+    ColltnAmt = ET.SubElement(Mndt, 'ColltnAmt') #mandate amount
+    
+    Cdtr = ET.SubElement(Mndt, 'Cdtr')
+    Nm_creditor_customer = ET.SubElement(Cdtr, 'Nm') #creditor name
+    
+    CdtrAcct = ET.SubElement(Mndt, 'CdtrAcct')
+    Id_1_creditor = ET.SubElement(CdtrAcct, 'Id')
+    Othr = ET.SubElement(Id_1_creditor, 'Othr')
+    Id_2_creditor = ET.SubElement(Othr, 'Id') #Utility Code HGBX00002000017848
+    
+    CdtrAgt = ET.SubElement(Mndt, 'CdtrAgt')
+    FinInstnId_creditor = ET.SubElement(CdtrAgt, 'FinInstnId')
+    ClrSysMmbId_creditor = ET.SubElement(FinInstnId_creditor, 'ClrSysMmbId')
+    MmbId_creditor = ET.SubElement(ClrSysMmbId_creditor, 'MmbId') #PUNB0HGB001
+    Nm_creditor_bank = ET.SubElement(FinInstnId_creditor, 'Nm') #SARVA HARYANA GRAMIN BANK
+    
+    Dbtr = ET.SubElement(Mndt, 'Dbtr')
+    Nm_debtor_customer = ET.SubElement(Dbtr, 'Nm')
+    #CtctDtls = ET.SubElement(Dbtr, 'CtctDtls')
+    #MobNb = ET.SubElement(CtctDtls, 'MobNb')
+    
+    DbtrAcct = ET.SubElement(Mndt, 'DbtrAcct')
+    Id_1_debtor = ET.SubElement(DbtrAcct, 'Id')
+    Othr_debtor = ET.SubElement(Id_1_debtor, 'Othr')
+    Id_2_debtor = ET.SubElement(Othr_debtor, 'Id') #Account number debtor
+    
+    Tp_debtor = ET.SubElement(DbtrAcct, 'Tp')
+    Prtry_debtor = ET.SubElement(Tp_debtor, 'Prtry') #Account type debtor
+    
+    DbtrAgt = ET.SubElement(Mndt, 'DbtrAgt')
+    FinInstnId_debtor = ET.SubElement(DbtrAgt, 'FinInstnId')
+    ClrSysMmbId_debtor = ET.SubElement(FinInstnId_debtor, 'ClrSysMmbId')
+    MmbId_debtor = ET.SubElement(ClrSysMmbId_debtor, 'MmbId') #debtor IFSC
+    #Nm_debtor_bank = ET.SubElement(FinInstnId_debtor, 'Nm') #debtor bank NAME
+    
+    Document.set('xmlns', 'urn:iso:std:iso:20022:tech:xsd:pain.009.001.01')
+    tree = t = ET.ElementTree(Document) #tree is created
+    
+    #hard coded values
+    MmbId1.text = 'PUNB0HGB001'
+    Nm_creditor_bank_head.text = 'SARVA HARYANA GRAMIN BANK'
+    MmbId_creditor.text = 'PUNB0HGB001'
+    Nm_creditor_bank.text = 'SARVA HARYANA GRAMIN BANK'
+    Prtry_S.text = 'L001'
+    Prtry_L.text = 'DEBIT'
+    Id_2_creditor.text = 'HGBX00002000017848'
+    SeqTp.text = 'RCUR'
+    Frqcy.text = 'MNTH'
+    ColltnAmt.set('Ccy', 'INR')
+    Nm_creditor_customer.text = 'SARVA HARYANA GRAMIN BANK'
+    
+    #variables
+    MndtReqId.text = MsgId.text = ref
+    CreDtTm.text = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=5, minutes=30))).replace(microsecond=0).isoformat() #system time in ISO format
+    ColltnAmt.text = data['amt']
+    FrDt.text = data['date']
+    FrstColltnDt.text = data['date1']
+    FnlColltnDt.text = data['date2']
+    Nm_debtor_customer.text = data['name']
+    Id_2_debtor.text = data['acc']
+    Prtry_debtor.text = data['type']
+    MmbId_debtor.text = MmbId2.text = data['ifsc']
+    
+    #indent and save the file
+    ET.indent(tree, space='\t')
+    tree.write(outfile, encoding='UTF-8', xml_declaration=True)
