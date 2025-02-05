e234a02d4a8116dfb58abb32315e303e49ad022b
grvOffice <mandate@shgbank.co.in>
2025-02-05 13:52:39 +0530

    form

--- mandate/forms.py
+++ mandate/forms.py
@@ -1,6 +1,6 @@
 from django.forms import ModelForm
 from django import forms
-from .models import Mandate
+from .models import Mandate, Frequency, Category, DebtorBank, DebtorAccType
 from django.contrib.admin.widgets import AdminDateWidget
 
 attrs_bs = {
@@ -14,6 +14,10 @@ attrs_bs_search = attrs_bs.copy()
 attrs_bs_search['data-live-search'] = 'true'
 
 class MandateForm(ModelForm):
+	category = forms.ModelChoiceField(Category.objects.all(), empty_label="", widget = forms.Select(attrs=attrs_bs))
+	frequency = forms.ModelChoiceField(Frequency.objects.all(), empty_label="", widget = forms.Select(attrs=attrs_bs))
+	debtor_bank = forms.ModelChoiceField(DebtorBank.objects.all(), empty_label="", widget = forms.Select(attrs=attrs_bs_search))
+	debtor_acc_type = forms.ModelChoiceField(DebtorAccType.objects.all(), empty_label="", widget = forms.Select(attrs=attrs_bs))
 
 	class Meta:
 		model = Mandate
@@ -36,16 +40,12 @@ class MandateForm(ModelForm):
 			"date_of_mandate": forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
 			"start_date": forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
 			"end_date": forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
-			"fixed_or_max": forms.RadioSelect(attrs={'class': 'form-check form-check-inline pt-7 pb-7'}),
-			"category": forms.Select(attrs=attrs_bs),
-			"frequency": forms.Select(attrs=attrs_bs),
-			"debtor_acc_type": forms.Select(attrs=attrs_bs),
-			"debtor_bank": forms.Select(attrs=attrs_bs_search),
-			"amount": forms.DateInput(attrs={'class': 'form-control', 'type': 'number', 'step': '0.01'}),
+			"fixed_or_max": forms.Select(attrs=attrs_bs),
+			"amount": forms.DateInput(attrs={'class': 'form-control', 'type': 'number', 'step': '0.01', 'min': '0.01', 'max': '10000000'}),
 			"name_of_debtor_account_holder": forms.DateInput(attrs={'class': 'form-control'}),
 			"debtor_legal_account_number": forms.DateInput(attrs={'class': 'form-control'}),
-			"debtor_account_number_ifsc": forms.DateInput(attrs={'class': 'form-control'}),
-			"credit_account": forms.DateInput(attrs={'class': 'form-control'}),
+			"debtor_account_number_ifsc": forms.DateInput(attrs={'class': 'form-control', 'pattern': '[A-Za-z]{4}\w{7}'}),
+			"credit_account": forms.DateInput(attrs={'class': 'form-control', 'maxlength': '14', 'pattern': '\d{4}\w{10}'}),
 		}
 
 
--- mandate/static/mandate/form_validate.js
+++ mandate/static/mandate/form_validate.js
@@ -1,7 +1,14 @@
-function validIcon(e) {
-    let elem_y = document.getElementById(e.target.id + '_y');
-    let elem_n = document.getElementById(e.target.id + '_n');
-    if (e.target.checkValidity()) {
+id_start_date.setAttribute('disabled', '');
+id_end_date.setAttribute('disabled', '');
+
+function validIcon(elem) {
+    if (elem.value == '') {
+        resetValidIcon(elem);
+        return;
+    }
+    let elem_y = document.getElementById(elem.id + '_y');
+    let elem_n = document.getElementById(elem.id + '_n');
+    if (elem.checkValidity()) {
         // valid form control/field
         if (!elem_n.classList.contains('d-none')) {
             elem_n.classList.add('d-none');
@@ -21,4 +28,62 @@ function validIcon(e) {
     }
 }
 
-form.addEventListener('change', validIcon);
+function resetValidIcon(elem) {
+    let elem_y = document.getElementById(elem.id + '_y');
+    let elem_n = document.getElementById(elem.id + '_n');
+    if (!elem_n.classList.contains('d-none')) {
+        elem_n.classList.add('d-none');
+    }
+    if (!elem_y.classList.contains('d-none')) {
+        elem_y.classList.add('d-none');
+    }
+}
+
+form.addEventListener('change', function(e) {
+    console.log(e.target.id);
+    if (e.target.type == 'radio') {
+        return;
+    }
+    validIcon(e.target);
+});
+
+id_date_of_mandate.addEventListener('change', dateChange);
+function dateChange(e) {
+    if (id_date_of_mandate.value == '') {
+        id_start_date.value = '';
+        id_start_date.dispatchEvent(new Event('change', {bubbles: true}));
+        id_start_date.setAttribute('disabled', '');
+        return;
+    }
+    id_start_date.setAttribute('min', id_date_of_mandate.value);
+    id_start_date.removeAttribute('disabled');
+    validIcon(id_start_date);
+}
+
+id_start_date.addEventListener('change', startDateChange);
+function startDateChange(e) {
+    if (id_start_date.value == '') {
+        id_end_date.value = '';
+        id_end_date.dispatchEvent(new Event('change', {bubbles: true}));
+        id_end_date.setAttribute('disabled', '');
+        return
+    }
+    id_end_date.setAttribute('min', id_start_date.value);
+    
+    let d = new Date(id_start_date.value);
+    d.setFullYear(d.getFullYear() + 40);
+    d.setDate(d.getDate() - 1);
+    // console.log(d.toISOString().split('T')[0]);
+    id_end_date.setAttribute('max', d.toISOString().split('T')[0]);
+
+    id_end_date.removeAttribute('disabled');
+    validIcon(id_end_date);
+}
+
+id_debtor_account_number_ifsc.addEventListener('change', function() {
+    this.value = this.value.toUpperCase();
+});
+
+id_credit_account.addEventListener('change', function() {
+    this.value = this.value.toUpperCase();
+});
