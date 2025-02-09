eb0c57dbbe53f40bfbfb88994c897e0e34407234
Gaurav <thegauravvv@gmail.com>
2025-02-10 01:49:52 +0530

    pdf

--- mandate/static/mandate/mypdf.js
+++ mandate/static/mandate/mypdf.js
@@ -1,4 +1,7 @@
 let { pdfjsLib } = globalThis;
+console.log(pdfjsLib.GlobalWorkerOptions.workerSrc)
+pdfjsLib.GlobalWorkerOptions.workerSrc = '/static/pdfjs/pdf.worker.min.mjs'
+console.log(pdfjsLib.GlobalWorkerOptions.workerSrc)
 
 let overlay = document.getElementById('overlay');
 overlay.style.display = 'none';
@@ -6,10 +9,37 @@ overlay.style.display = 'none';
 let prev = document.getElementById('prev');
 let next = document.getElementById('next');
 let sel = document.getElementById('sel');
+let thumbContainer = document.getElementById('thumb-container');
+
+let currentPage = 1;
+let dim = {
+    height: 400,
+    width: 300
+};
+let doc;
+
+async function pdf_init(file) {
+    overlay.style.display = '';
+    try {
+        doc = await getDoc(file);
+        console.log('inside try of pdf_init\n' + doc);
+    } catch (err) {
+        throw err;
+    }
+    
+    console.log(doc.numPages);
+    showPage(1);
+}
 
 async function getDoc(pdf) {
     let doc_promise = pdfjsLib.getDocument(pdf).promise;
-    let doc = await doc_promise;
+    let doc;
+    try {
+        doc = await doc_promise;
+    } catch (err) {
+        throw err;
+    }
+    
     return doc;
 };
 
@@ -39,21 +69,6 @@ function clearElem(elem) {
     for (let child of elem.querySelectorAll('canvas')) child.remove();
 }
 
-let currentPage = 1;
-let dim = {
-    height: 400,
-    width: 300
-};
-let thumbContainer = document.getElementById('thumb-container');
-let doc;
-
-async function pdf_init(file) {
-    overlay.style.display = '';
-    doc = await getDoc(file);
-    console.log(doc.numPages);
-    showPage(1);
-}
-
 async function showPage(page_num) {
     clearElem(thumbContainer);
     let canvas = await pageRender(doc, page_num, dim);
@@ -66,9 +81,22 @@ let input = document.getElementById('input');
 input.addEventListener('change', async function(){
     let file = input.files[0];
     overlay.style.display = '';
-    file.arrayBuffer().then(function(file_arrayBuffer){
-        pdf_init(file_arrayBuffer);
-    });
+    file.arrayBuffer().then(
+        async function(file_arrayBuffer) {
+            try {
+                await pdf_init(file_arrayBuffer);
+            } catch (err) {
+                alert(err);
+                input.value = '';
+                clearElem(thumbContainer);
+                overlay.style.display = 'none';
+            }
+        },
+        function (err) {
+            alert('err');
+            input.value = '';
+        }
+    );
 });
 
 prev.addEventListener('click', prevPage);
