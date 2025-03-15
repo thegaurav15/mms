2000d8fd6b19b68fb7e59cc21b64f6f9cf485647
grvOffice <mandate@shgbank.co.in>
2025-03-15 13:06:04 +0530

    after gitignore

--- mandate/static/mandate/paginate.js
+++
@@ -1,5 +0,0 @@
-let nav = document.getElementById('nav');
-
-for (elem of nav.querySelectorAll('a')) {
-    elem.setAttribute('href', elem.getAttribute('href') + (new URL(window.location.href)).search);
-}
